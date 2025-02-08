b4827cc273962014063a568ee7224ca808448f20
JVH <Joni1234@hotmail.dk>
2025-02-08 20:49:37 +0100

    Cosmoclick clicker begin

--- source/g_cosmoclick.cpp
+++ source/g_cosmoclick.cpp
@@ -13,6 +13,7 @@ using Money = NamedType<u32, struct MoneyTag, Arithmetic, FormatLongNumber>;
 using Cost = NamedType<u32, struct CostTag, Arithmetic, FormatLongNumber>;
 using Income = NamedType<u32, struct IncomeTag, Arithmetic, FormatLongNumber>;
 struct Building {
+    String name;
     Cost cost;
     Income production;
 };
@@ -21,27 +22,36 @@ struct GameData {
 };
 enum class ValueUnitTextSize : u8 { small, normal, larger };
 enum class Unit : u8 { cosmos, cosmos_per_second };
-struct ValueUnit {
+constexpr String UnitToString(const Unit unit) {
+    switch (unit) {
+        case Unit::cosmos:
+            return "¤";
+        case Unit::cosmos_per_second:
+            return "¤-s";
+    }
+    return "unknown unit";
+}
+template <class T> struct ValueUnit {
     NodeTree& tree;
-    NodeHandleOptional node_handle;
-    NodeHandleOptional value_handle;
-    NodeHandleOptional unit_handle;
-    constexpr void SetValue(const String& value);
-    constexpr void SetUnit(Unit unit);
+    NodeHandleOptional node_handle { };
+    NodeHandleOptional value_handle { };
+    NodeHandleOptional unit_handle { };
 
-    ValueUnit(NodeTree& tree, NodeHandle parent_handle, const String& value, Unit unit, SDL_Color text_color, FontSizes font_sizes);
+    ValueUnit(NodeTree& tree, NodeHandle parent_handle, const T& value, Unit unit, SDL_Color text_color, FontSizes font_sizes);
+    constexpr void SetValue(const T& value);
+    constexpr void SetUnit(Unit unit);
 };
 struct GameFrame {
     NodeTree tree;
     GameFrame();
-    [[nodiscard]] constexpr b8 InsidePlanet(uint2 screen_position) { return tree.GetStyle(planet.GetHandle()).IsInside(screen_position); }
-    void SetMoney (Money new_money){ money->SetValue(std::format("{}", new_money)); }
-    void SetIncome (Income new_income){ income->SetValue(std::format("{}", new_income)); }
+    [[nodiscard]] constexpr b8 InsidePlanet(uint2 screen_position);
+    [[nodiscard]] constexpr ValueUnit<Money>& GetMoneyValueUnit();
+    [[nodiscard]] constexpr ValueUnit<Income>& GetIncomeValueUnit();
 
 private:
     NodeHandleOptional planet;
-    std::unique_ptr<ValueUnit> money;
-    std::unique_ptr<ValueUnit> income;
+    std::unique_ptr<ValueUnit<Money>> money;
+    std::unique_ptr<ValueUnit<Income>> income;
 };
 enum class Scene { game, quit };
 class CosmoClick {
@@ -101,11 +111,21 @@ Scene CosmoClick::GameScene() {
             constexpr Money click_money = Money { 5U };
             game_data.money += click_money;
             game_frame.tree.MarkDirty();
-            game_frame.SetMoney(game_data.money);
-            game_frame.SetIncome(Income { 0U });
+            game_frame.GetMoneyValueUnit().SetValue(game_data.money);
+            game_frame.GetIncomeValueUnit().SetValue(Income { 0U });
         }
     }
     return Scene::game;
+}
+
+// ui
+template <class T> constexpr void ValueUnit<T>::SetValue(const T& value) { tree.GetProperties(value_handle.GetHandle()).text = std::format("{}", value); }
+template <class T> constexpr void ValueUnit<T>::SetUnit(Unit unit) { tree.GetProperties(value_handle.GetHandle()).text = UnitToString(unit); }
+template <class T> ValueUnit<T>::ValueUnit(NodeTree& tree, NodeHandle parent_handle, const T& value, const Unit unit, const SDL_Color text_color, const FontSizes font_sizes): tree { tree } {
+    node_handle = B(tree, parent_handle, hug).Alignment(top_right).Build();
+    value_handle = B(tree, node_handle.GetHandle(), hug).Text("", font_sizes).Fill(text_color).Build();
+    unit_handle = B(tree, node_handle.GetHandle(), hug).Text(UnitToString(unit), static_cast<FontSizes>(static_cast<f32>(font_sizes) * 0.66F)).Fill(text_color).Build();
+    SetValue(value);
 }
 
 NodeHandle BuildItem(NodeTree& tree, NodeHandle parent_handle, const String& name) {
@@ -113,33 +133,20 @@ NodeHandle BuildItem(NodeTree& tree, NodeHandle parent_handle, const String& nam
     const NodeHandle upper = B(tree, build_item, { fill, hug }).Center().GapAuto().Build();
     const NodeHandle lower = B(tree, build_item, { fill, hug }).Center().GapAuto().Build();
     B(tree, upper, hug).Text("0000", FontSizes::title).Fill(colors::black).Build();
+    ValueUnit<Cost>(tree, upper, Cost { 0U }, Unit::cosmos, colors::black, FontSizes::h1);
     B(tree, lower, hug).Text(name, FontSizes::title).Fill(colors::black).Build();
-    ValueUnit(tree, upper, "2000", Unit::cosmos, colors::black, FontSizes::h1);
-    ValueUnit(tree, lower, "10", Unit::cosmos_per_second, colors::black, FontSizes::h1);
+    ValueUnit<Income>(tree, lower, Income { 0U }, Unit::cosmos_per_second, colors::black, FontSizes::h1);
     return build_item;
 }
-constexpr String UnitToString(const Unit unit) {
-    switch (unit) {
-        case Unit::cosmos:
-            return "¤";
-        case Unit::cosmos_per_second:
-            return "¤-s";
-    }
-    return "unknown unit";
-}
-ValueUnit::ValueUnit(NodeTree& tree, NodeHandle parent_handle, const String& value, const Unit unit, const SDL_Color text_color, const FontSizes font_sizes) : tree { tree } {
-    node_handle = B(tree, parent_handle, hug).Alignment(top_right).Build();
-    value_handle = B(tree, node_handle.GetHandle(), hug).Text(value, font_sizes).Fill(text_color).Build();
-    unit_handle = B(tree, node_handle.GetHandle(), hug).Text(UnitToString(unit), static_cast<FontSizes>(static_cast<f32>(font_sizes) * 0.66F)).Fill(text_color).Build();
-}
-constexpr void ValueUnit::SetValue(const String& value) { tree.GetProperties(value_handle.GetHandle()).text = value; }
-constexpr void ValueUnit::SetUnit(const Unit unit) { tree.GetProperties(unit_handle.GetHandle()).text = UnitToString(unit); }
+constexpr b8 GameFrame::InsidePlanet(uint2 screen_position) { return tree.GetStyle(planet.GetHandle()).IsInside(screen_position); }
+constexpr ValueUnit<Money>& GameFrame::GetMoneyValueUnit() { return *money.get(); }
+constexpr ValueUnit<Income>& GameFrame::GetIncomeValueUnit() { return *income.get(); }
 GameFrame::GameFrame() {
     const NodeHandle frame = B(tree, fill, uint2 { 0U, 0U }).Build();
     const NodeHandle game = B(tree, frame, fill).Direction(vertical).Center().Build();
     const NodeHandle title = B(tree, game, hug).Fill(colors::white).Text("Cosmo Click", FontSizes::title).Build();
-    money = std::make_unique<ValueUnit>(tree, game, "1000", Unit::cosmos, colors::white, FontSizes::h1);
-    income = std::make_unique<ValueUnit>(tree, game, "1000", Unit::cosmos_per_second, colors::white, FontSizes::h4);
+    money = std::make_unique<ValueUnit<Money>>(tree, game, Money { 0U }, Unit::cosmos, colors::white, FontSizes::h1);
+    income = std::make_unique<ValueUnit<Income>>(tree, game, Income { 0U }, Unit::cosmos_per_second, colors::white, FontSizes::h4);
     const NodeHandle click = B(tree, game, fill).Padding2(uint2 { 0U, 100U }).Fill(colors::dark_grey).Alignment(top_center).Build();
     constexpr u32 planet_size = 200U;
     constexpr u32 planet_border_size = 10U;
